2e5dd87016529b1d303a9105fcd8f1267e837e07
Han Liang Wee Eric <Eric.aka.darth_vader@hotmail.com>
2016-09-28 17:12:19 +0800

    Updated Code with new shiny kernels

--- prediction/compare.py
+++ prediction/compare.py
@@ -1,6 +1,5 @@
 from math import sqrt
-from sklearn import gaussian_process
-from sklearn import cross_validation
+from sklearn import gaussian_process as gp
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeRegressor
@@ -13,6 +12,7 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import mean_squared_error
 from inputs import read_train_dev_files
 from plotting import plot_bar, plot_all_Y
+import numpy as np
 import sys
 
 x_train_file_name = "data/splitted/X/urop/trainX.txt"
@@ -31,6 +31,8 @@ elif len(sys.argv) == 3:
 
 X_train, y_train, X_dev, y_dev = read_train_dev_files(x_train_file_name, x_dev_file_name, y_train_file_name, y_dev_file_name)
 
+n_feats = len(X_train[0])
+
 classifiers = {
     "Nearest Neighbors": KNeighborsClassifier(2),
     "Linear SVM": SVC(kernel="linear"),
@@ -39,13 +41,23 @@ classifiers = {
     "Random Forest": RandomForestClassifier(n_estimators=10, min_samples_split=1024, max_features=60, max_depth=20),
     "AdaBoost": AdaBoostClassifier(),
     "Naive Bayes": GaussianNB(),
-    "GP abs_exp": gaussian_process.GaussianProcess(corr='absolute_exponential'),
-    "GP squared_exp": gaussian_process.GaussianProcess(corr='squared_exponential'),
+    "Bagging with DTRegg": BaggingRegressor(DecisionTreeRegressor(min_samples_split=1024, max_depth=20, max_features=60)),
+    "GP isotropic RBF": gp.GaussianProcessRegressor(kernel=gp.kernels.RBF()),
+    "GP anisotropic RBF": gp.GaussianProcessRegressor(kernel=gp.kernels.RBF(length_scale=np.array([1]*n_feats))),
+    "GP isotropic matern nu=0.5": gp.GaussianProcessRegressor(kernel=gp.kernels.Matern(nu=0.5)),
+    "GP isotropic matern nu=1.5": gp.GaussianProcessRegressor(kernel=gp.kernels.Matern(nu=1.5)),
+    "GP isotropic matern nu=2.5": gp.GaussianProcessRegressor(kernel=gp.kernels.Matern(nu=2.5)),
+    "GP dot product": gp.GaussianProcessRegressor(kernel=gp.kernels.DotProduct()),
+#  3-th leading minor not positive definite
+#    "GP exp sine squared": gp.GaussianProcessRegressor(kernel=gp.kernels.ExpSineSquared()),
+    "GP rational quadratic": gp.GaussianProcessRegressor(kernel=gp.kernels.RationalQuadratic()),
+    "GP white kernel": gp.GaussianProcessRegressor(kernel=gp.kernels.WhiteKernel()),
+    "GP abs_exp": gp.GaussianProcess(corr='absolute_exponential'),
+    "GP squared_exp": gp.GaussianProcess(corr='squared_exponential'),
     #General Expo doesn't work with the data: 'Exception: Length of theta must be 2 or 525'
-    "GP cubic": gaussian_process.GaussianProcess(corr='cubic'),
-    "GP linear": gaussian_process.GaussianProcess(corr='linear'),
-    "Bagging with DTRegg": BaggingRegressor(DecisionTreeRegressor(min_samples_split=1024, max_depth=20, max_features=60),
-                                            n_estimators=10, max_samples=1.0, max_features=1.0)}
+    "GP cubic": gp.GaussianProcess(corr='cubic'),
+    "GP linear": gp.GaussianProcess(corr='linear')
+}
 
 models_rmse = {}
 for name, model in classifiers.items():
